672517a9a4ad33d2fc2bfb138bc008fbb75cbcef
Oshal Borkar <mail@oshalborkar.dev>
2025-06-15 01:41:15 +0530

    First boot and minimal kernel

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/opt/local/bin/bash
+set -xue
+
+# QEMU file path
+QEMU=/opt/local/bin/qemu-system-riscv32
+
+# Path to clang and compiler flags
+CC=/opt/local/bin/clang-mp-20
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+# Start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
